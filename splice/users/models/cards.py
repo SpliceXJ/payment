8cf447bb8dcee591d891f20ae61a6d5aeb80a1be
shurlz <johnegbeola10@gmail.com>
2023-04-21 14:22:47 +0100

    gateway integrated

--- splice/users/models/cards.py
+++ splice/users/models/cards.py
@@ -5,7 +5,7 @@ from splice.users.models.user import SpliceUser
 
 
 class UsersCard(models.Model):
-    """user can only have 3 cards stored, be sure to add that check"""
+    """user can only have 3 cards stored, sure to add that check"""
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     owner = models.ForeignKey(
@@ -13,7 +13,6 @@ class UsersCard(models.Model):
     )
     authorization = models.CharField(max_length=80)
     email = models.EmailField(max_length=200)
-    first_four_digits = models.CharField(max_length=4)
     last_four_digits = models.CharField(max_length=4)
     card_type = models.CharField(max_length=15)
 
@@ -25,7 +24,6 @@ class UsersCard(models.Model):
         owner: SpliceUser,
         authorization: str,
         email: str,
-        first_four_digits: Union[str, int],
         last_four_digits: Union[str, int],
         card_type: str,
     ) -> "UsersCard":
@@ -33,7 +31,6 @@ class UsersCard(models.Model):
             owner=owner,
             authorization=authorization,
             email=email,
-            first_four_digits=first_four_digits,
             last_four_digits=last_four_digits,
             card_type=card_type,
         )
